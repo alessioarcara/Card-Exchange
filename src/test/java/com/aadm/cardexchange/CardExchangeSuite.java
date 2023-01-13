e5ccd69c8b51cb8cb279111ee0a69b165b1daf31
Matteo Sacco <matteosacco@outlook.it>
2023-01-13 22:30:54 +0100

    MapDB collections init with associated data from JSONParse and json files input.
    Creation of a mock test

--- src/test/java/com/aadm/cardexchange/CardExchangeSuite.java
+++ src/test/java/com/aadm/cardexchange/CardExchangeSuite.java
@@ -2,6 +2,7 @@ package com.aadm.cardexchange;
 
 import com.aadm.cardexchange.client.HomeActivityTest;
 import com.aadm.cardexchange.server.CardServiceTest;
+import com.aadm.cardexchange.server.DBInitializeTest;
 import com.aadm.cardexchange.server.JSONParserTest;
 import com.aadm.cardexchange.shared.CardTest;
 import com.aadm.cardexchange.shared.MagicCardDecoratorTest;
@@ -17,6 +18,7 @@ import org.junit.platform.suite.api.Suite;
         PokemonCardDecoratorTest.class,
         MagicCardDecoratorTest.class,
         JSONParserTest.class,
+        DBInitializeTest.class,
         CardServiceTest.class,
         HomeActivityTest.class,
 })
