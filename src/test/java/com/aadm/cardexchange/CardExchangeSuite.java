c258b08f03e83d405b3d89c1f2e400b82eff6ccf
alessioarcara <Alessio.arcara@studio.unibo.it>
2023-01-14 10:29:35 +0100

    Revert "[UC01#4] Initialization of MapDB database"

--- src/test/java/com/aadm/cardexchange/CardExchangeSuite.java
+++ src/test/java/com/aadm/cardexchange/CardExchangeSuite.java
@@ -2,7 +2,6 @@ package com.aadm.cardexchange;
 
 import com.aadm.cardexchange.client.HomeActivityTest;
 import com.aadm.cardexchange.server.CardServiceTest;
-import com.aadm.cardexchange.server.DBInitializeTest;
 import com.aadm.cardexchange.server.JSONParserTest;
 import com.aadm.cardexchange.shared.CardTest;
 import com.aadm.cardexchange.shared.MagicCardDecoratorTest;
@@ -18,7 +17,6 @@ import org.junit.platform.suite.api.Suite;
         PokemonCardDecoratorTest.class,
         MagicCardDecoratorTest.class,
         JSONParserTest.class,
-        DBInitializeTest.class,
         CardServiceTest.class,
         HomeActivityTest.class,
 })
