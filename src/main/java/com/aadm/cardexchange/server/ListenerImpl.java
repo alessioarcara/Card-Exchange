4018e4eb845d5eb198157b1308bc2561a94227ab
alessioarcara <Spammer123@>
2023-01-14 03:20:10 +0100

    Resolved any conflicts with the development branch.

--- src/main/java/com/aadm/cardexchange/server/ListenerImpl.java
+++ src/main/java/com/aadm/cardexchange/server/ListenerImpl.java
@@ -15,62 +15,10 @@ import java.util.Map;
 
 
 public class ListenerImpl implements ServletContextListener, MapDBConstants {
-    /*
-    private static final MagicCardDecorator[] MAGIC_DUMMY_DATA = new MagicCardDecorator[]{
-            new MagicCardDecorator(new
-                    CardImpl("magicName", "magicDesc", "magicType"),
-                    "magicArtist", "rare",
-                    true, true, true, true, true
-            ),
-            new MagicCardDecorator(new
-                    CardImpl("magicName", "magicDesc", "magicType"),
-                    "magicArtist", "epic",
-                    true, true, true, true, true
-            ),
-            new MagicCardDecorator(new
-                    CardImpl("magicName", "magicDesc", "magicType"),
-                    "magicArtist", "legendary",
-                    true, true, true, true, true
-            ),
-    };
-    private static final PokemonCardDecorator[] POKEMON_DUMMY_DATA = new PokemonCardDecorator[]{
-            new PokemonCardDecorator(new
-                    CardImpl("pokemonName", "pokemonDesc", "pokemonType"),
-                    "pokemonArtist", "http://www.image.jpg", "rare",
-                    true, true, true, true, true
-            ),
-            new PokemonCardDecorator(new
-                    CardImpl("pokemonName", "pokemonDesc", "pokemonType"),
-                    "pokemonArtist", "http://www.image.jpg", "rare",
-                    true, true, true, true, true
-            ),
-            new PokemonCardDecorator(new
-                    CardImpl("pokemonName", "pokemonDesc", "pokemonType"),
-                    "pokemonArtist", "http://www.image.jpg", "rare",
-                    true, true, true, true, true
-            ),
-    };
-    private static final YuGiOhCardDecorator[] YUGIOH_DUMMY_DATA = new YuGiOhCardDecorator[]{
-            new YuGiOhCardDecorator(new
-                    CardImpl("yugiohName", "yugiohDesc", "yugiohType"),
-                    "race", "http://www.image.jpg", "http://www.smallimage.jpg"
-            ),
-            new YuGiOhCardDecorator(new
-                    CardImpl("yugiohName", "yugiohDesc", "yugiohType"),
-                    "race", "http://www.image.jpg", "http://www.smallimage.jpg"
-            ),
-            new YuGiOhCardDecorator(new
-                    CardImpl("yugiohName", "yugiohDesc", "yugiohType"),
-                    "race", "http://www.image.jpg", "http://www.smallimage.jpg"
-            ),
-    };
+    private static final String MAGIC_FILE_PATH = "./resources/json/magic_cards.json";
+    private static final String POKEMON_FILE_PATH = "./resources/json/magic_cards.json";
+    private static final String YUGIOH_FILE_PATH = "./resources/json/magic_cards.json";
 
-    public void loadDummyData(int counter, Map<Integer, CardDecorator> map, CardDecorator[] DUMMY_DATA) {
-        for (CardDecorator card : DUMMY_DATA) {
-            map.put(counter++, card);
-        }
-    }
-    */
 
     private void uploadDataToDB(int counter, Map<Integer, CardDecorator> map, CardDecorator[] data) {
         for (CardDecorator card : data) {
@@ -82,55 +30,29 @@ public class ListenerImpl implements ServletContextListener, MapDBConstants {
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("Context initialized.");
         System.out.println("*** Loading data from file. ***");
-
         Gson gson = new Gson();
         GsonSerializer<CardDecorator> cardSerializer = new GsonSerializer<>(gson);
+        JSONParser parser = new JSONParser(new MagicCardParseStrategy(), gson);
         MapDB DB = new MapDBImpl();
-        int counter = 0;
 
-        Map<Integer, CardDecorator> yuGiOhMap = DB.getCachedMap(sce.getServletContext(), YUGIOH_MAP_NAME,
-                Serializer.INTEGER, cardSerializer);
         Map<Integer, CardDecorator> magicMap = DB.getCachedMap(sce.getServletContext(), MAGIC_MAP_NAME,
                 Serializer.INTEGER, cardSerializer);
         Map<Integer, CardDecorator> pokemonMap = DB.getCachedMap(sce.getServletContext(), POKEMON_MAP_NAME,
                 Serializer.INTEGER, cardSerializer);
+        Map<Integer, CardDecorator> yuGiOhMap = DB.getCachedMap(sce.getServletContext(), YUGIOH_MAP_NAME,
+                Serializer.INTEGER, cardSerializer);
 
-        JSONParser parser = new JSONParser(new YuGiOhCardParseStrategy(), gson);
+        int count = 0;
         try {
-            uploadDataToDB(counter, yuGiOhMap, parser.parseJSON("./resources/json/yugioh_cards.json"));
-
-            parser.setParseStrategy(new MagicCardParseStrategy());
-            uploadDataToDB(counter, magicMap, parser.parseJSON("./resources/json/magic_cards.json"));
-
+            uploadDataToDB(count, magicMap, parser.parseJSON(MAGIC_FILE_PATH));
             parser.setParseStrategy(new PokemonCardParseStrategy());
-            uploadDataToDB(counter, pokemonMap, parser.parseJSON("./resources/json/pokemon_cards.json"));
+            uploadDataToDB(count, yuGiOhMap, parser.parseJSON(POKEMON_FILE_PATH));
+            parser.setParseStrategy(new YuGiOhCardParseStrategy());
+            uploadDataToDB(count, pokemonMap, parser.parseJSON(YUGIOH_FILE_PATH));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
         System.out.println("*** Data Loaded. ***");
-
-        /*
-        loadDummyData(counter, mMap, MAGIC_DUMMY_DATA);
-        loadDummyData(counter, pMap, POKEMON_DUMMY_DATA);
-        loadDummyData(counter, yMap, YUGIOH_DUMMY_DATA);
-
-//        if (!new File(DB_FILENAME).exists()) {
-//            try {
-//                // List<String> stocks = new ArrayList<>();
-//                // ObjectMapper objectMapper = new ObjectMapper();
-//                // stocks = objectMapper.readValue(new File("stock.json"), new
-//                // TypeReference<List<String>>() {
-//                // });
-//            } catch (Exception e) {
-//                Logger.getGlobal().warning(e.toString());
-//            }
-//        }
-        // Gson gson = new Gson();
-        // Person p = gson.fromJson("{\"age\": 24, \"name\":\"Mario\"}", Person.class);
-        // System.out.println("Data: " + p);
-
-         */
     }
 
     @Override
