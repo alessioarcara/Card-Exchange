4e332f4766758c7128253f7146735398abe691ad
Matteo Sacco <matteosacco@outlook.it>
2023-01-15 16:25:07 +0100

    update graphics for card component

--- src/main/java/com/aadm/cardexchange/client/widgets/CardWidget.java
+++ src/main/java/com/aadm/cardexchange/client/widgets/CardWidget.java
@@ -28,40 +28,38 @@ public class CardWidget extends Composite {
     public CardWidget(FunctionInterface parent, CardDecorator card) {
         initWidget(uiBinder.createAndBindUi(this));
         nameDiv.setInnerHTML(card.getName());
-        descDiv.setInnerHTML(card.getDescription());
-        typeDiv.setInnerHTML(card.getType());
+        descDiv.setInnerHTML("<b>Description:</b><br>" + card.getDescription());
+        typeDiv.setInnerHTML("<b>Type:</b><br>" + card.getType());
+        imageDiv.setPixelSize(90, 131);
 
         if (card instanceof YuGiOhCardDecorator) {
             imageDiv.setUrl(((YuGiOhCardDecorator) card).getSmallImageUrl());
-            details.setInnerHTML("Race: " + ((YuGiOhCardDecorator) card).getRace());
+            details.setInnerHTML("<b>Race</b>: " + ((YuGiOhCardDecorator) card).getRace());
         }
         if (card instanceof PokemonCardDecorator) {
             imageDiv.setUrl(((PokemonCardDecorator) card).getImageUrl());
-            String val = "<br>Artist: " + ((PokemonCardDecorator) card).getArtist();
-            val += "<br>Rarity: " + ((PokemonCardDecorator) card).getRarity();
-            val += "<br>" + (((PokemonCardDecorator) card).getIsFirstEdition() ? "<b>First edition</b>" : "<s>First edition</s>");
-            val += "<br>" + (((PokemonCardDecorator) card).getIsHolo() ? "<b>Holo</b>" : "<s>Holo</s>");
-            val += "<br>" + (((PokemonCardDecorator) card).getIsNormal() ? "<b>Normal</b>" : "<s>Normal</s>");
-            val += "<br>" + (((PokemonCardDecorator) card).getIsReverse() ? "<b>Reverse</b>" : "<s>Reverse</s>");
-            val += "<br>" + (((PokemonCardDecorator) card).getIsPromo() ? "<b>Promo</b>" : "<s>Promo</s>");
+            String val = "<br><b>Artist</b>: " + ((PokemonCardDecorator) card).getArtist();
+            val += "<br><b>Rarity</b>: " + ((PokemonCardDecorator) card).getRarity();
+            val += (((PokemonCardDecorator) card).getIsFirstEdition() ? "<br><b>First edition</b>" : "");
+            val += (((PokemonCardDecorator) card).getIsHolo() ? "<br><b>Holo</b>" : "");
+            val += (((PokemonCardDecorator) card).getIsNormal() ? "<br><b>Normal</b>" : "");
+            val += (((PokemonCardDecorator) card).getIsReverse() ? "<br><b>Reverse</b>" : "");
+            val += (((PokemonCardDecorator) card).getIsPromo() ? "<br><b>Promo</b>" : "");
             details.setInnerHTML(val);
         }
         if (card instanceof MagicCardDecorator) {
-            String val = "<br>Artist: " + ((MagicCardDecorator) card).getArtist();
-            val += "<br>Rarity: " + ((MagicCardDecorator) card).getRarity();
-            val += "<br>" + (((MagicCardDecorator) card).getHasFoil() ? "<b>Foil</b>" : "<s>Foil</s>");
-            val += "<br>" + (((MagicCardDecorator) card).getIsAlternative() ? "<b>Alternative</b>" : "<s>Alternative</s>");
-            val += "<br>" + (((MagicCardDecorator) card).getIsFullArt() ? "<b>Full Art</b>" : "<s>Full Art</s>");
-            val += "<br>" + (((MagicCardDecorator) card).getIsPromo() ? "<b>Promo</b>" : "<s>Promo</s>");
+            String val = "<br><b>Artist</b>: " + ((MagicCardDecorator) card).getArtist();
+            val += "<br><b>Rarity</b>: " + ((MagicCardDecorator) card).getRarity();
+            val += (((MagicCardDecorator) card).getHasFoil() ? "<br><b>Foil</b>" : "");
+            val += (((MagicCardDecorator) card).getIsAlternative() ? "<br><b>Alternative</b>" : "");
+            val += (((MagicCardDecorator) card).getIsFullArt() ? "<br><b>Full Art</b>" : "");
+            val += (((MagicCardDecorator) card).getIsPromo() ? "<br><b>Promo</b>" : "");
             details.setInnerHTML(val);
             imageDiv.setPixelSize(0, 0);
         }
 
         detailsButton.addClickHandler(clickEvent -> parent.handleClickCard());
 
-        if (!(card instanceof MagicCardDecorator)) {
-            imageDiv.setPixelSize(90, 131);
-        }
     }
 
     interface CardUIBinder extends UiBinder<Widget, CardWidget> {
