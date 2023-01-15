43a8d8cf005c65c8593fa02ebb854e99ddc568a0
Matteo Sacco <matteosacco@outlook.it>
2023-01-15 09:20:29 +0100

    Creation of card widget with all details

--- src/main/java/com/aadm/cardexchange/client/widgets/CardWidget.java
+++ src/main/java/com/aadm/cardexchange/client/widgets/CardWidget.java
@@ -1,13 +1,14 @@
 package com.aadm.cardexchange.client.widgets;
 
 import com.aadm.cardexchange.shared.models.CardDecorator;
+import com.aadm.cardexchange.shared.models.MagicCardDecorator;
+import com.aadm.cardexchange.shared.models.PokemonCardDecorator;
+import com.aadm.cardexchange.shared.models.YuGiOhCardDecorator;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.PushButton;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.*;
 
 public class CardWidget extends Composite {
     private static final CardUIBinder uiBinder = GWT.create(CardUIBinder.class);
@@ -17,6 +18,12 @@ public class CardWidget extends Composite {
     DivElement descDiv;
     @UiField
     DivElement typeDiv;
+
+    @UiField
+    DivElement details;
+
+    @UiField
+    Image imageDiv;
     @UiField
     PushButton detailsButton;
 
@@ -24,8 +31,41 @@ public class CardWidget extends Composite {
         initWidget(uiBinder.createAndBindUi(this));
         nameDiv.setInnerHTML(card.getName());
         descDiv.setInnerHTML(card.getDescription());
-        typeDiv.setInnerHTML(card.getDescription());
+        typeDiv.setInnerHTML(card.getType());
+
+        if (card instanceof YuGiOhCardDecorator) {
+            imageDiv.setUrl(((YuGiOhCardDecorator) card).getSmallImageUrl());
+            details.setInnerHTML("Race: " + ((YuGiOhCardDecorator) card).getRace());
+        }
+        if (card instanceof PokemonCardDecorator) {
+            imageDiv.setUrl(((PokemonCardDecorator) card).getImageUrl());
+            String val = "<br>Artist: " + ((PokemonCardDecorator) card).getArtist();
+            val += "<br>Rarity: " + ((PokemonCardDecorator) card).getRarity();
+            val += "<br>First edition: " + ((PokemonCardDecorator) card).getIsFirstEdition();
+            val += "<br>Holo: " + ((PokemonCardDecorator) card).getIsHolo();
+            val += "<br>Normal: " + ((PokemonCardDecorator) card).getIsNormal();
+            val += "<br>Reverse: " + ((PokemonCardDecorator) card).getIsReverse();
+            val += "<br>Promo: " + ((PokemonCardDecorator) card).getIsPromo();
+            details.setInnerHTML(val);
+        }
+        if (card instanceof MagicCardDecorator) {
+            String val = "<br>Artist: " + ((MagicCardDecorator) card).getArtist();
+            val += "<br>Rarity: " + ((MagicCardDecorator) card).getRarity();
+            val += "<br>Foil: " + ((MagicCardDecorator) card).getHasFoil();
+            val += "<br>Alternative: " + ((MagicCardDecorator) card).getIsAlternative();
+            val += "<br>Full Art: " + ((MagicCardDecorator) card).getIsFullArt();
+            val += "<br>Promo: " + ((MagicCardDecorator) card).getIsPromo();
+            details.setInnerHTML(val);
+        }
+
+
         detailsButton.addClickHandler(clickEvent -> parent.handleClickCard());
+        if (!(card instanceof MagicCardDecorator)) {
+            imageDiv.setPixelSize(69, 100);
+        } else {
+            imageDiv.setPixelSize(0, 0);
+        }
+
     }
 
     interface CardUIBinder extends UiBinder<Widget, CardWidget> {
