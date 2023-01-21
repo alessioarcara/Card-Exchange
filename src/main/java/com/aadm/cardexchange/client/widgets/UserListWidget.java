a67e80fa78652d40d73229f70d726e9907f15381
Alessia Crimaldi <alessiacrimaldi@virgilio.it>
2023-01-21 14:50:26 +0100

    added `Exchange` button and refactored UserListWidget using GWT FlexTable for dynamic users lists instead of HTMLTable

--- src/main/java/com/aadm/cardexchange/client/widgets/UserListWidget.java
+++ src/main/java/com/aadm/cardexchange/client/widgets/UserListWidget.java
@@ -5,31 +5,41 @@ import com.google.gwt.dom.client.*;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Widget;
 
 public class UserListWidget extends Composite {
     private static final UserListUIBinder uiBinder = GWT.create(UserListUIBinder.class);
     @UiField
     HeadingElement tableHeading;
-    @UiField
-    TableSectionElement tableBody;
+    @UiField(provided = true)
+    FlexTable table;
 
     @UiConstructor public UserListWidget(String title, String[] users) {
+        setupTable();
         initWidget(uiBinder.createAndBindUi(this));
-        tableHeading.setInnerHTML(title);
+        tableHeading.setInnerText(title);
         for (String user: users) {
-            Element tr = DOM.createTR();
-            Element td1 = DOM.createTD();
-            td1.setAttribute("colspan", "2");
-            Element td2 = DOM.createTD();
-            td1.setInnerHTML(user);
-            td2.setInnerHTML("1 (Ottimo)");
-            DOM.appendChild(tr, td1);
-            DOM.appendChild(tr, td2);
-            DOM.appendChild(tableBody, tr);
+            addRow(user);
         }
+    }
+
+    private void setupTable() {
+        table = new FlexTable();
+        TableSectionElement tHead = ((TableElement) ((Element) table.getElement())).createTHead();
+        TableRowElement row = tHead.insertRow(0);
+        row.insertCell(0).setInnerText("User");
+        row.insertCell(1).setInnerText("State");
+        row.insertCell(2).setInnerText("");
+    }
+
+    private void addRow(String email) {
+        int numRows = (table.getRowCount());
+        table.setText(numRows, 0, email);
+        table.setText(numRows, 1, "1 (Ottimo)");
+        table.setWidget(numRows, 2, new Button("Exchange"));
     }
 
     interface UserListUIBinder extends UiBinder<Widget, UserListWidget> {
