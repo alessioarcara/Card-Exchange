05b15db84b1be831c238281777d9168756c69c5c
Matteo Sacco <matteosacco@outlook.it>
2023-02-04 17:23:44 +0100

    Naming refactor

--- src/main/java/com/aadm/cardexchange/server/services/ExchangeServiceImpl.java
+++ src/main/java/com/aadm/cardexchange/server/services/ExchangeServiceImpl.java
@@ -4,20 +4,18 @@ import com.aadm.cardexchange.server.gsonserializer.GsonSerializer;
 import com.aadm.cardexchange.server.mapdb.MapDB;
 import com.aadm.cardexchange.server.mapdb.MapDBConstants;
 import com.aadm.cardexchange.server.mapdb.MapDBImpl;
-import com.aadm.cardexchange.shared.DeckService;
 import com.aadm.cardexchange.shared.ExchangeService;
 import com.aadm.cardexchange.shared.exceptions.AuthException;
 import com.aadm.cardexchange.shared.exceptions.InputException;
-import com.aadm.cardexchange.shared.models.*;
-import com.google.common.reflect.TypeToken;
+import com.aadm.cardexchange.shared.models.PhysicalCardImpl;
+import com.aadm.cardexchange.shared.models.Proposal;
+import com.aadm.cardexchange.shared.models.User;
 import com.google.gson.Gson;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.mapdb.Serializer;
 
-import java.lang.reflect.Type;
-import java.util.*;
-
-import static com.aadm.cardexchange.server.services.AuthServiceImpl.validateEmail;
+import java.util.List;
+import java.util.Map;
 
 
 public class ExchangeServiceImpl extends RemoteServiceServlet implements ExchangeService, MapDBConstants {
@@ -30,7 +28,7 @@ public class ExchangeServiceImpl extends RemoteServiceServlet implements Exchang
     public ExchangeServiceImpl(MapDB mockDB) {
         db = mockDB;
     }
-    private boolean checkEmailExistance(String email) {
+    private boolean checkEmailExistence(String email) {
         Map<String, User> userMap = db.getPersistentMap(
                 getServletContext(), USER_MAP_NAME, Serializer.STRING, new GsonSerializer<>(gson));
         System.out.println(userMap.get(email) );
@@ -38,7 +36,7 @@ public class ExchangeServiceImpl extends RemoteServiceServlet implements Exchang
             return true;
         } else return false;
     }
-    private boolean checkPcardListConsistency(List<PhysicalCardImpl> physicalCards) {
+    private boolean checkPCardListConsistency(List<PhysicalCardImpl> physicalCards) {
        if (physicalCards == null) return false;
        else return !physicalCards.isEmpty();
     }
@@ -46,12 +44,12 @@ public class ExchangeServiceImpl extends RemoteServiceServlet implements Exchang
     public boolean addProposal(String token, String receiverUserEmail, List<PhysicalCardImpl> senderPhysicalCards, List<PhysicalCardImpl> receiverPhysicalCards) throws AuthException, InputException {
         String email = AuthServiceImpl.checkTokenValidity(token,
                 db.getPersistentMap(getServletContext(), LOGIN_MAP_NAME, Serializer.STRING, new GsonSerializer<>(gson)));
-        if (receiverUserEmail == null || receiverUserEmail.isEmpty() || !checkEmailExistance(receiverUserEmail)) {
+        if (receiverUserEmail == null || receiverUserEmail.isEmpty() || !checkEmailExistence(receiverUserEmail)) {
             throw new InputException("Invalid receiverUserEmail name");
-        } else if (!checkPcardListConsistency(senderPhysicalCards)) {
+        } else if (!checkPCardListConsistency(senderPhysicalCards)) {
             throw new InputException("Invalid senderPhysicalCards list");
-        } else if (!checkPcardListConsistency(receiverPhysicalCards)) {
-            throw new InputException("Invalid senderPhysicalCards list");
+        } else if (!checkPCardListConsistency(receiverPhysicalCards)) {
+            throw new InputException("Invalid receiverPhysicalCards list");
         } else {
             Proposal newProposal = new Proposal(email, receiverUserEmail, senderPhysicalCards, receiverPhysicalCards);
             Map<Integer, Proposal> proposalMap = db.getPersistentMap(getServletContext(), PROPOSAL_MAP_NAME, Serializer.INTEGER, new GsonSerializer<>(gson));
